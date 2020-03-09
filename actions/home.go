57d699ccf2ad4f1a49451ab2a8af0fc54237ae19
Saharak <Saharakmanoo@outlook.com>
2020-03-09 11:14:53 +0700

    [Fixed] code

--- actions/home.go
+++ actions/home.go
@@ -1,9 +1,13 @@
 package actions
 
 import "github.com/gobuffalo/buffalo"
+import "fmt"
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
-	return c.Render(200, r.JSON(map[string]string{"message": "Welcome to Buffalo!"}))
+	fmt.Println("Hello, Saharak")
+	fmt.Println("Hello, 世界")
+
+	return c.Render(200, r.JSON(map[string]string{"message": "Welcome to Buffalo! dove"}))
 }
